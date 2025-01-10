05869c87470eab16461363733a101c336c79a640
michaela.kantorova@gmail.com <michaela.kantorova@gmail.com>
2025-01-10 13:59:54 +0100

    jednoduchy export kolekcie ako csv pouzitim JS

---
+++ BlazorApp/wwwroot/js/download.js
@@ -0,0 +1,8 @@
+﻿function downloadFile(csvContent, filename) {
+    var blob = new Blob([csvContent], { type: 'text/csv' });
+    var link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = filename;
+    link.click();
+    URL.revokeObjectURL(link.href);
+}
